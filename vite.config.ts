b2cb9807968556089ce0041da2a82193d2e3ad9e
kata671 <barbiebarbara480@gmail.com>
2025-09-19 18:45:08 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
-  plugins: [react()],
-  base: '/skin-scribe-suite/',   // ważne dla GitHub Pages
-  build: { outDir: 'dist' }
+  base: '/skin-scribe-suite/',
+  // ...
 })
